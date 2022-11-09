37baac1a0a753897968a58a6d8a17b850b465226
Benjamin Ryan (redraskal) <ben@ryben.dev>
2022-11-09 17:50:17 -0600

    chore: format code

--- web/pages/capture.tsx
+++ web/pages/capture.tsx
@@ -1,14 +1,14 @@
 import Camera from "../components/Camera";
 
 export default function Capture() {
-    const onCapture = (img: string, text: string[]) => {
-        text.forEach(line => console.log(line));
-        // TODO
-    };
+	const onCapture = (img: string, text: string[]) => {
+		text.forEach((line) => console.log(line));
+		// TODO
+	};
 
-    return (
-        <>
-            <Camera onCapture={onCapture} />
-        </>
-    );
+	return (
+		<>
+			<Camera onCapture={onCapture} />
+		</>
+	);
 }
--- web/pages/contacts.tsx
+++ web/pages/contacts.tsx
@@ -1,3 +1,3 @@
 export default function Contacts() {
-    return <></>;
+	return <></>;
 }
